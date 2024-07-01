fe7755807bc6efdd10be442cb4aaffa421d01b23
DaAlbrecht <dadidi42@gmail.com>
2024-07-02 01:13:48 +0200

    feat: demo spell

--- src/combat/mod.rs
+++ src/combat/mod.rs
@@ -1,4 +1,7 @@
-use bevy::prelude::*;
+use bevy::{
+    prelude::*,
+    sprite::{MaterialMesh2dBundle, Mesh2dHandle},
+};
 use leafwing_input_manager::action_state::ActionState;
 
 use crate::{input::PlayerAction, player::Player, AppState};
@@ -11,10 +14,31 @@ impl Plugin for CombatPlugin {
     }
 }
 
-fn test(query: Query<&ActionState<PlayerAction>, With<Player>>) {
+fn test(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    query: Query<&ActionState<PlayerAction>, With<Player>>,
+    player: Query<Entity, With<Player>>,
+) {
     let action_state = query.single();
 
     if action_state.just_pressed(&PlayerAction::Ability1) {
         println!("Ability 1 just pressed");
+
+        let player = player.single();
+
+        let fire_ball = commands
+            .spawn((
+                MaterialMesh2dBundle {
+                    mesh: Mesh2dHandle(meshes.add(Circle::new(50.0))),
+                    material: materials.add(Color::RED),
+                    ..default()
+                },
+                Name::new("Ability1"),
+            ))
+            .id();
+
+        commands.entity(player).add_child(fire_ball);
     }
 }
